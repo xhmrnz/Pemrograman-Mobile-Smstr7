1820e633f0049cf8dde90e462d2ae9131dae29bb
lhmrnfrzrfr <ilhamriana3@gmail.com>
2019-12-16 16:19:25 +0700

    update deleted project

--- MyButterKnife/app/src/main/java/com/ilham/mybutterknife/Contact.java
+++
@@ -1,4 +0,0 @@
-package com.ilham.mybutterknife;
-
-public class Contact {
-}
--- MyFootballLeague/app/src/main/java/com/ilham/myfootballleague/League.kt
+++
@@ -1,9 +0,0 @@
-package com.ilham.myfootballleague
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class League(
-    val logo: Int?,val name: String?, val description: String?
-) : Parcelable
